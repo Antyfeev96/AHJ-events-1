f65f3849211df580d501e4e27b7ec57356b207d9
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-03-02 20:26:12 +0300

    File-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,9 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
+            options: {
+              name: 'src/images/[name].[ext]',
+            },
           },
         ],
       },
